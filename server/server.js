e1f331f1a1cb791993f3e7d8d57fcd8c8cca5a7b
julie-mac <123594716+julie-mac@users.noreply.github.com>
2023-08-17 01:13:21 -0400

    editing scripts

--- server/server.js
+++ server/server.js
@@ -35,34 +35,3 @@ app.get('*', (req, res) => {  // <-- Add this wildcard route
 });
 
 app.listen(config.app.port, () => console.log(`Server listening on port ${config.app.port}`));v
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const config = require('./config/config');
-// const app = express();
-// const mongoose = require('mongoose');
-// const PORT = (process.env.PORT || config.app.port);
-
-// mongoose.connect((process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/forum_db'), {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => {
-//     console.log('MongoDB connected…');
-// })
-// .catch(err => {
-//     console.log('Error connecting to the database', err);
-//     process.exit(1);
-// });
-
-// app.use(cors());
-// app.use(express.json()); // for parsing application/json
-
-// // Use the routes defined in your routes folder
-// const routes = require('./routes');
-// app.use(routes);
-// const topicsRouter = require('./routes/topics');
-// app.use('/topics', topicsRouter);
-
-// app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
